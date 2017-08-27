ef9fc2055a113d5572b3046b03165157c6175202
Ramprasath Babu <ramprasath.technology@gmail.com>
2017-08-27 14:23:01 -0500

    creating container WeatherList for displaying weather data

--- src/reducers/weather-reducer.js
+++ src/reducers/weather-reducer.js
@@ -1,4 +1,12 @@
-export default function(state = null, action){
-    console.log('action received', action);
+import React from 'react';
+import FETCH_WEATHER from '../actions/index';
+
+export default function(state = [], action){
+    switch(action.type){        
+        case "FETCH_WEATHER":
+        return [ action.payload.data, ...state];        
+    }
+
+   
     return state;
 }
