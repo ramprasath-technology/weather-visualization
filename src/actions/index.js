db116a80b5e6226a6305e78636764b30dd533cf4
Ramprasath Babu <ramprasath.technology@gmail.com>
2018-01-30 15:20:14 -0600

    updating API key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '2336a691c593f40198a65383bd5894a3';
+const API_KEY = '<insert your API key>';
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/search-bar';
 import WeatherList from '../containers/weather-list';
+import Navigation from '../components/navigation';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Navigation />
         <SearchBar />
         <WeatherList />
       </div>
